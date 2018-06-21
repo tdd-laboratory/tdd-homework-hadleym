57591f4224773ab77d5c8af4c6c6d334f2e7ffc3
Mark Hadley <mark.hadley@evernym.com>
2018-06-21 14:13:45 -0600

    Adds 10 Failing Tests

--- library_test.py
+++ library_test.py
@@ -28,6 +28,21 @@ class TestCase(unittest.TestCase):
     def test_integers(self):
         self.assert_extract(NUM_CORPUS, library.integers, '1845', '15', '20', '80')
 
+    def test_comma_notated_long_integers(self):
+        self.assert_extract('numbers 123,456,789 end numbers', library.integers, '123456789')
+
+    def test_comma_sperated_list_integers(self):
+        self.assert_extract('numbers 123, 456, 789 end numbers', library.integers, '123456789')
+
+    def test_comma_sperated_integer(self):
+        self.assert_extract('numbers 1,456,789 end numbers', library.integers, '1456789')
+
+    def test_comma_sperated_integer_with_decimal(self):
+        self.assert_extract('numbers 1,456,789.12 end numbers', library.integers, '1456789')
+
+    def test_comma_british(self):
+        self.assert_extract('numbers 1.234 end of line', library.integers, '1234')
+
     # Third unit test; prove that if we look for integers where there are none, we get no results.
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
@@ -36,12 +51,27 @@ class TestCase(unittest.TestCase):
     def test_dates(self):
         self.assert_extract("something 2015-12-25.", library.dates_iso8601, '2015-12-25')
 
+    def test_dates_timestamp(self):
+        self.assert_extract("something 2015-12-25 18:22:19.12", library.dates_iso8601, '2015-12-25 18:22:19.12')
+
+    def test_dates_with_T(self):
+        self.assert_extract("something 2015-12-25T18:22:19.12", library.dates_iso8601, '2015-12-25 12:22:19.12')
+
+    def test_dates_offset(self):
+        self.assert_extract("something 2015-12-25T18:22:19.12 -0800", library.dates_iso8601, '2015-12-25 10:22:19.12')
+
+    def test_dates_UTC(self):
+        self.assert_extract("something 2015-12-25 18:22:19.12 UTC", library.dates_iso8601, '2015-12-25 10:22:19.12')
+
     # Test that months greater than 12 are ignored as dates
     def test_no_dates(self):
         self.assert_extract("something 2015-13-25 not ending", library.dates_iso8601)
 
     def test_word_dates(self):
         self.assert_extract("something 25 Jan 2017.", library.dates_wordy, '25 Jan 2017')
+
+    def test_word_dates_comma(self):
+        self.assert_extract("something 25 Jan, 2017.", library.dates_wordy, '25 Jan 2017')
 
 if __name__ == '__main__':
     unittest.main()
